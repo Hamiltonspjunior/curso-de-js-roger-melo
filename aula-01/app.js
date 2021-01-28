9a7669f93b610844045595c44e4cd368f796d0a5
Hamiltonspjunior <hamiltonspjunior@gmail.com>
2021-01-28 13:33:33 -0300

    Add aula 1 e 2 e resoluçao dos exercicios 1 e 2

---
+++ aula-01/app.js
@@ -0,0 +1,38 @@
+// Váriaveis e constantes
+
+// Atribuição de valores
+let age = 25
+let currentYear = 2021
+
+console.log(age, currentYear)
+
+// Reatribuição de valores
+age = 32
+
+console.log(age)
+
+
+var score = 30 // Forma antiga de declarar váriaveis
+
+console.log(score)
+
+// Constantes
+const points = 100
+points = 40
+
+console.log(points)
+
+/*
+   RESUMO:
+      - Váriaveis e constantes são usadas para armazenar valores dentro de uma aplicação.
+
+      - Podemos declarar uma váriavel utilizando a palavra let ou var(Método antigo)
+
+      - Podemos alterar o valor de uma variável a qualquer momento reatribuindo um novo valor
+
+      - Constantes são declaradas utilizando a palavra const, e não podem sofrer reatribuição
+
+      - Existem atualmente 8 tipos de dados em JS
+
+      - JS é uma linguagem dinamicamente tipada
+*/
